2c26fcc22038761f2ebbb0704abcccedf348e1b2
Robin Unger <robin.e.unger@gmail.com>
2019-11-04 18:51:47 -0600

    Add flight selector

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,18 @@
 class FlightsController < ApplicationController
+
   def index
     @airports = Airport.all
     @flight_dates = Flight.pluck(Arel.sql("distinct date(departure_time)")).sort
 
-    @flights = Flight.where("start_airport_id = :from_id AND 
-                             finish_airport_id = :to_id AND 
-                             departure_time LIKE :date",
-                             from_id: params[:flight][:from_id],
-                             to_id: params[:flight][:to_id],
-                             date: "%#{params[:flight][:date]}%")
-
+    unless params[:flight].nil?
+      @flights = Flight.where("start_airport_id = :from_id AND 
+                              finish_airport_id = :to_id AND 
+                              departure_time LIKE :date",
+                              from_id: params[:flight][:from_id],
+                              to_id: params[:flight][:to_id],
+                              date: "%#{params[:flight][:date]}%")
+    end
   end
+
 end
 
