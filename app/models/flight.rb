3fecc62d49b42cc9f272c18055c8b18ef6cc00ff
Robin Unger <robin.e.unger@gmail.com>
2019-11-05 16:50:25 -0600

    Add models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,6 @@
 class Flight < ApplicationRecord
   has_many :airports
+  has_many :bookings
   accepts_nested_attributes_for :airports
   belongs_to :start_airport, class_name: "Airport"
   belongs_to :finish_airport, class_name: "Airport"
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_224627) do
+ActiveRecord::Schema.define(version: 2019_11_05_222719) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -27,6 +34,15 @@ ActiveRecord::Schema.define(version: 2019_10_29_224627) do
     t.datetime "updated_at", null: false
     t.index ["finish_airport_id"], name: "index_flights_on_finish_airport_id"
     t.index ["start_airport_id"], name: "index_flights_on_start_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.integer "booking_id"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
