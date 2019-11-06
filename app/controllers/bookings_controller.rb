7e8631f28e39ef287275e906e6d8ad0ca7354096
Robin Unger <robin.e.unger@gmail.com>
2019-11-06 15:47:41 -0600

    Add booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,11 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
     @flight = Flight.find(params[:flight][:id])
+    @passengers = params[:flight][:passengers].to_i
   end
 
   def create
+
   end
 end
