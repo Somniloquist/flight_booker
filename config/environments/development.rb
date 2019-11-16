5015f073c3ee74ef0a7c79497c990f72c9132232
Robin Unger <robin.e.unger@gmail.com>
2019-11-16 15:26:45 -0600

    Add letter_opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  # Set mailer delivery method to litter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -58,4 +61,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
