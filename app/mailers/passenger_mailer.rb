1c030cce21d61113bedd552319e568f5cf4642e3
Robin Unger <robin.e.unger@gmail.com>
2019-11-18 14:04:40 -0600

    Allow PassengerMailer to be called from command line

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,7 +1,7 @@
 class PassengerMailer < ApplicationMailer
 
-  def booking_confirmation_email
-    @passenger = params[:passenger]
+  def booking_confirmation_email(passenger = nil)
+    @passenger = passenger || params[:passenger]
     @url = "http://example.com/login"
     mail(to: @passenger.email, subject: "Welcome to my awesome site")
   end
